e27c24b82e6cec67a8df078358292b7eae12bb9d
SujiKim <136331819+SujiKim-hattoo@users.noreply.github.com>
2025-06-30 12:09:29 +0900

    Time: 0 ms (100%), Space: 17.8 MB (49.95%) - LeetHub

--- 3778-transform-array-by-parity/3778-transform-array-by-parity.py
+++ 3778-transform-array-by-parity/3778-transform-array-by-parity.py
@@ -1,14 +1,11 @@
 class Solution:
     def transformArray(self, nums: List[int]) -> List[int]:
-        # Replace each even number with 0 and each odd number with 1.
-        for i in range(0, len(nums)):
+        for i in range(len(nums)):
             if nums[i] % 2 == 0:
                 nums[i] = 0
-                print(nums)
             else:
                 nums[i] = 1
-                print(nums)
-        
+
         return sorted(nums)
         
         
