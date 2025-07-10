6b00e3a0c4a76bb557dd1e5ae68bc05fba0f9f7f
SujiKim <136331819+SujiKim-hattoo@users.noreply.github.com>
2025-07-10 17:33:46 +0900

    Time: 0 ms (100%), Space: 17.7 MB (66.36%) - LeetHub

--- 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/3872-find-most-frequent-vowel-and-consonant.py
@@ -7,18 +7,13 @@ class Solution:
             if char in ['a', 'e', 'i', 'o', 'u']:  # vowel
                 if char not in vowel_dic:
                     vowel_dic[char] = 1
-                    print(vowel_dic)
                 else:
                     vowel_dic[char] += 1
-                    print(vowel_dic)
 
             else:  # consonant
                 if char not in consonant_dic:
                     consonant_dic[char] = 1
-                    print(consonant_dic)
-
                 else:
                     consonant_dic[char] += 1
-                    print(consonant_dic)
 
-        return max(vowel_dic.values()) + max(consonant_dic.values())
+        return max(vowel_dic.values(), default = 0) + max(consonant_dic.values(), default = 0)
