e50859e90f1fd120d63941fbae0cd64465308118
SujiKim <136331819+SujiKim-hattoo@users.noreply.github.com>
2025-07-15 23:58:29 +0900

    Time: 6 ms (37.47%), Space: 17.7 MB (84.69%) - LeetHub

--- 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.py
+++ 2917-count-pairs-whose-sum-is-less-than-target/2917-count-pairs-whose-sum-is-less-than-target.py
@@ -1,12 +1,36 @@
 class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
         count = 0
-         
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if (i >= j) or (nums[i] + nums[j] >= target):
-                    continue
-                else:
-                    count += 1
+        nums.sort()
         
+        for i in range(len(nums)):
+            j = i + 1
+            while (j < len(nums)) and (nums[i] + nums[j] < target):
+                count += 1
+                j += 1
         return count
+
+
+        # <second try> _ while loop Index Error(out of range)
+        # for i in range(len(nums)):
+        #     j = i + 1
+        #     if j == len(nums):
+        #         break
+        #     else:
+        #         while nums[i] + nums[j] < target:
+        #             count += 1
+        #             j += 1
+
+        # return count  
+        
+        # <first try> _ Memory 17.72MB
+        # count = 0
+         
+        # for i in range(len(nums)):
+        #     for j in range(len(nums)):
+        #         if (i >= j) or (nums[i] + nums[j] >= target):
+        #             continue
+        #         else:
+        #             count += 1
+        
+        # return count
